ecd6ec2adff3a33140109424cb0e844d1b6dc805
Akhil Jain <akhiljda@gmail.com>
2026-01-15 22:31:34 +0530

    changes

---
+++ Day1.py
@@ -0,0 +1,39 @@
+age = 34.5
+print(age)  
+print(type(age))  
+
+age = 'Akhil'
+print(type(age))
+
+
+is_exist = False
+print(is_exist)
+print(type(is_exist))
+
+
+firstname = 'c'
+print(type(firstname))
+
+description = "Akhil jain is taeaching Python" \
+"He is teaching on Podtest!" \
+"We have 50+ students"
+
+#docstrings
+para = """   
+          Akhil Jain is teaching
+          he is teaching
+          we have 50 students
+"""
+
+
+print(para)
+prizes = None
+print(type(prizes))
+
+prizes = 6
+print(type(prizes))
+
+
+
+
+
